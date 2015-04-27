a8898d0745ae278e0f1cb5133968176e1790f6b0
Jerry <jerry.wasdyke@gmail.com>
2015-04-26 20:28:16 -0400

    commit 2

--- bulk_order.py
+++ bulk_order.py
@@ -2,19 +2,34 @@
 
 import csv
 
-with open('TestOrderData.csv', 'rb') as f:
-    reader = csv.reader(f)
+def bulk_orders(account_name):
+    # same account
+    # same symbol
+    # if time between child orders are <= 60 seconds
+    print "test"
 
-    rownum = 0
-    for row in reader:
-        if rownum == 0:
-            header = row
-        else:
-            colnum = 0
-            for col in row:
-                print '%-8s: %s' % (header[colnum], col)
-                colnum += 1
+def main():
+    with open('TestOrderData.csv', 'rb') as f:
+        reader = csv.reader(f)
 
-        rownum +=1
+        rownum = 0
+        # Iterate through rows
+        for row in reader:
+            if( rownum != 0 ):
+                this_acct = row[2]
+                if( this_acct == prev_acct ):
+                    # stuff
 
-f.close()
+                else:
+
+
+
+
+            elif( rownum == 0 ):
+                header = row
+
+            rownum +=1
+
+        f.close()
+
+main()
